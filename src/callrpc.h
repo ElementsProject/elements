7146a9703195388451306ab7ee4233af011e6a83
Matt Corallo <git@bluematt.me>
2014-12-06 14:37:48 -0800

    MOVEONLY: Move CallRPC into its own file

---
+++ src/callrpc.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CALLRPC_H
+#define BITCOIN_CALLRPC_H
+
+#include <string>
+#include <stdexcept>
+
+#include <univalue.h>
+
+static const char DEFAULT_RPCCONNECT[] = "127.0.0.1";
+static const int DEFAULT_HTTP_CLIENT_TIMEOUT=900;
+
+//
+// Exception thrown on connection error.  This error is used to determine
+// when to wait if -rpcwait is given.
+//
+class CConnectionFailed : public std::runtime_error
+{
+public:
+
+    explicit inline CConnectionFailed(const std::string& msg) :
+        std::runtime_error(msg)
+    {}
+
+};
+
+UniValue CallRPC(const std::string& strMethod, const UniValue& params);
+
+#endif // BITCOIN_CALLRPC_H
