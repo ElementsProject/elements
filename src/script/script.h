3c6faaa5da1a7afee3e019b1f2bdf93a80d5c719
Matt Corallo <git@bluematt.me>
2014-12-19 04:08:15 -0500

    Add getint64 to CScriptNum

--- src/script/script.h
+++ src/script/script.h
@@ -292,6 +292,11 @@ public:
         return m_value;
     }
 
+    int64_t getint64() const
+    {
+        return m_value;
+    }
+
     std::vector<unsigned char> getvch() const
     {
         return serialize(m_value);
