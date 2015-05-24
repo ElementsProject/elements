5b4281056ecee07eb4dddd981a96e28c2262aa6b
Jorge Timón <jtimon@jtimon.cc>
2015-05-24 23:35:55 +0200

    Chainparams: Generic selection with -chain=<chainString> in addition of -testnet and -regtest

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -101,7 +101,7 @@ bool AppInit(int argc, char* argv[])
             fprintf(stderr,"Error reading configuration file: %s\n", e.what());
             return false;
         }
-        // Check for -testnet or -regtest parameter (Params() calls are only valid after this clause)
+        // Check for -chain, -testnet or -regtest parameter (cGlobalChainBaseParams.Get() calls are only valid after this clause)
         try {
             SelectParams(ChainNameFromCommandLine());
         } catch(std::exception &e) {
