2affed549aae40ce80521c2ac0409b2f22505872
Mark Friedenbach <mark@friedenbach.org>
2015-06-01 17:52:34 -0700

    Move SEQUENCE_THRESHOLD to script/script.h
    Will now be needed by CHECKSEQUENCEVERIFY code.

--- src/main.h
+++ src/main.h
@@ -72,8 +72,6 @@ static const unsigned int BLOCKFILE_CHUNK_SIZE = 0x1000000; // 16 MiB
 static const unsigned int UNDOFILE_CHUNK_SIZE = 0x100000; // 1 MiB
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
-/** Threshold for inverted CTxIn::nSequence: below this value it is interpreted as a relative lock-time, otherwise ignored. */
-static const uint32_t SEQUENCE_THRESHOLD = (1 << 31);
 /** Maximum number of script-checking threads allowed */
 static const int MAX_SCRIPTCHECK_THREADS = 16;
 /** -par default (number of script-checking threads, 0 = auto) */
