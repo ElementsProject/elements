05871c0392e8bde436f06621a844e961a0e01552
Pieter Wuille <pieter.wuille@gmail.com>
2015-06-09 17:30:47 +0200

    Produce confidential addresses in GUI

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -383,7 +383,7 @@ QString AddressTableModel::addRow(const QString &type, const QString &label, con
                 return QString();
             }
         }
-        strAddress = CBitcoinAddress(newKey.GetID()).ToString();
+        strAddress = CBitcoinAddress(newKey.GetID()).AddBlindingKey(wallet->blinding_pubkey).ToString();
     }
     else
     {
