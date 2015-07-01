d16d0760537ef7e95a005f4b88d1a22ed7c09e5a
Jorge Timón <jtimon@jtimon.cc>
2015-07-01 19:02:51 +0200

    Checkpoints: The hash of the genesis block it's the genesis checkpoint and chain id

--- src/chainparams.h
+++ src/chainparams.h
@@ -53,6 +53,11 @@ public:
         MAX_BASE58_TYPES
     };
 
+    /**
+     * Maps strNetworkID [see BIP70] to chainID (hashGenesisBlock and genesis checkpoint)
+     */
+    static const std::map<std::string, uint256> supportedChains;
+
     const Consensus::Params& GetConsensus() const { return consensus; }
     const CMessageHeader::MessageStartChars& MessageStart() const { return pchMessageStart; }
     int GetDefaultPort() const { return nDefaultPort; }
