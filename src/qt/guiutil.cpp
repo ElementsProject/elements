d0cccad9d418c1a2fb574fd6e85cd90215608a49
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-12 23:40:02 +0000

    Recognise temporary REPLACE_BY_FEE service bit

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -835,6 +835,9 @@ QString formatServicesStr(quint64 mask)
             case NODE_XTHIN:
                 strList.append("XTHIN");
                 break;
+            case NODE_REPLACE_BY_FEE:
+                strList.append("REPLACE_BY_FEE?");
+                break;
             default:
                 strList.append(QString("%1[%2]").arg("UNKNOWN").arg(check));
             }
