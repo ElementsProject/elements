5412a584f42e8bcc3d73f6e2424b0ec1363d1e6e
instagibbs <gsanders87@gmail.com>
2016-04-21 11:11:04 -0700

    Add thread-local storage of rpc username

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -17,7 +17,12 @@
 
 #include <boost/function.hpp>
 
+#include <boost/thread/tss.hpp>
+
 #include <univalue.h>
+
+//Thread-local rpc user name for logging purposes
+extern boost::thread_specific_ptr<std::string> userInstance;
 
 class CRPCCommand;
 
