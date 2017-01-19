201e73205d25b58f41b6d27b3552f7071e2e7569
instagibbs <gsanders87@gmail.com>
2017-01-19 18:03:35 -0500

    f'Add regtest python peg-in test'

--- qa/rpc-tests/pegging.py
+++ qa/rpc-tests/pegging.py
@@ -17,12 +17,6 @@ print(sys.argv[2])
 fedpeg_key="cPxqWyf1HDGpGFH1dnfjz8HbiWxvwG8WXyetbuAiw4thKXUdXLpR"
 fedpeg_pubkey="512103dff4923d778550cc13ce0d887d737553b4b58f4e8e886507fc39f5e447b2186451ae"
 
-pak_key_1="cPrpBrgbpmkNGYayVBkNNrRsjyWqJK61W7yC2EnJNob98ZhAzuFm"
-pak_pubkey_1="03701c7d314219f76d193c8762be96a2f68c1b387e3cc00dcc7020e13b1670bfdb"
-
-pak_key_2="cW2b8y2Vp1QD3YkrQL1bpT589DQbaRBMjRZgmz6R8ykzFni7cocX"
-pak_pubkey_2="03b86dae93129f64dfc3ae5eeca0be0cb9e439b9d23d18c08c8f7358136c0d8ce4"
-
 bitcoin_datadir="/tmp/"+''.join(random.choice('0123456789ABCDEF') for i in range(5))
 bitcoin_pass=''.join(random.choice('0123456789ABCDEF') for i in range(10))
 sidechain_datadir="/tmp/"+''.join(random.choice('0123456789ABCDEF') for i in range(5))
@@ -56,6 +50,7 @@ with open(os.path.join(sidechain_datadir, "elements.conf"), 'w') as f:
         f.write("txindex=1\n")
         f.write("fedpegscript="+fedpeg_pubkey+"\n")
         f.write("daemon=1\n")
+        f.write("blockprioritysize=1000000\n")
         f.write("mainchainrpchost=127.0.0.1\n")
         f.write("mainchainrpcport="+str(bitcoin_port)+"\n")
         f.write("mainchainrpcuser=bitcoinrpc\n")
