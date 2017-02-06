f28d2d1533785fba22fd61993b3463723b748e62
Gregory Sanders <gsanders87@gmail.com>
2017-02-06 15:26:28 -0500

    listunspent: don't list outputs with unknown amounts

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2492,6 +2492,9 @@ UniValue listunspent(const UniValue& params, bool fHelp)
             continue;
 
         CAmount nValue = out.tx->GetValueOut(out.i);
+        if (nValue == -1)
+            continue;
+
         UniValue entry(UniValue::VOBJ);
         entry.push_back(Pair("txid", out.tx->GetHash().GetHex()));
         entry.push_back(Pair("vout", out.i));
