44ceb66a1522ee36c2ab85647e56508a3a63c1d1
Jorge Timón <jtimon@jtimon.cc>
2017-03-08 09:42:52 +0100

    CA: Globals: Introduce global CAssetsDir gAssetsDir

---
+++ src/global/common.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2017 The Elements Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_GLOBAL_COMMON_H
+#define BITCOIN_GLOBAL_COMMON_H
+
+#include <string>
+#include <vector>
+
+class CAssetsDir;
+
+extern const CAssetsDir& gAssetsDir;
+
+void InitGlobalAssetDir(const std::vector<std::string>& assetsToInit);
+
+#endif // BITCOIN_GLOBAL_COMMON_H
