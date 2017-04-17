0d7c6d4f7fc22a051807bc418dd7dee6fdd3e2ac
Gregory Sanders <gsanders87@gmail.com>
2017-04-17 15:45:13 -0400

    Disable changePosition arguments functionality, only used in fundraw

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2868,7 +2868,7 @@ UniValue fundrawtransaction(const JSONRPCRequest& request)
                             "2. options                 (object, optional)\n"
                             "   {\n"
                             "     \"changeAddress\"          (string, optional, default pool address) The bitcoin address to receive the change\n"
-                            "     \"changePosition\"         (numeric, optional, default random) The index of the change output\n"
+                            "     \"changePosition\"    (numeric, optional, default random) The index of the change output (DISABLED)\n"
                             "     \"includeWatching\"        (boolean, optional, default false) Also select inputs which are watch only\n"
                             "     \"lockUnspents\"           (boolean, optional, default false) Lock selected unspent outputs\n"
                             "     \"reserveChangeKey\"       (boolean, optional, default true) Reserves the change output key from the keypool\n"
@@ -2885,7 +2885,6 @@ UniValue fundrawtransaction(const JSONRPCRequest& request)
                             "{\n"
                             "  \"hex\":       \"value\", (string)  The resulting raw transaction (hex-encoded string)\n"
                             "  \"fee\":       n,         (numeric) Fee in " + CURRENCY_UNIT + " the resulting transaction pays\n"
-                            "  \"changepos\": n          (numeric) The position of the added change output, or -1\n"
                             "}\n"
                             "\nExamples:\n"
                             "\nCreate a transaction with no inputs\n"
@@ -2942,7 +2941,8 @@ UniValue fundrawtransaction(const JSONRPCRequest& request)
         }
 
         if (options.exists("changePosition"))
-            changePosition = options["changePosition"].get_int();
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "changePosition argument is disabled");
+            //changePosition = options["changePosition"].get_int();
 
         if (options.exists("includeWatching"))
             includeWatching = options["includeWatching"].get_bool();
@@ -2994,7 +2994,6 @@ UniValue fundrawtransaction(const JSONRPCRequest& request)
 
     UniValue result(UniValue::VOBJ);
     result.push_back(Pair("hex", EncodeHexTx(tx)));
-    result.push_back(Pair("changepos", changePosition));
     result.push_back(Pair("fee", ValueFromAmount(nFeeOut)));
 
     return result;
