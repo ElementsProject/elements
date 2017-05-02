ea9622bb4771e54b03b2e238e96927dc667da6c5
Gregory Sanders <gsanders87@gmail.com>
2017-05-02 14:11:55 -0400

    Handle TX_FEE case in IsMine

--- src/script/ismine.cpp
+++ src/script/ismine.cpp
@@ -64,6 +64,7 @@ isminetype IsMine(const CKeyStore &keystore, const CScript& scriptPubKey, bool& 
     case TX_NONSTANDARD:
     case TX_NULL_DATA:
     case TX_WITHDRAW_LOCK:
+    case TX_FEE:
         break;
     case TX_PUBKEY:
         keyID = CPubKey(vSolutions[0]).GetID();
