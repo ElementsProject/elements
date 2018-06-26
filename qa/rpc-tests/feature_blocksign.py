26d75a252ea6a4a27c5e905f87a306e8d7c4cde0
Jorge Timón <jtimon@jtimon.cc>
2018-06-27 00:14:26 +0200

    1 signer

--- qa/rpc-tests/feature_blocksign.py
+++ qa/rpc-tests/feature_blocksign.py
@@ -121,4 +121,4 @@ class BlockSignTest(test_framework.BitcoinTestFramework):
         self.check_height(101)
 
 if __name__ == '__main__':
-    BlockSignTest(num_nodes=9, required_signers=7).main()
+    BlockSignTest(num_nodes=1, required_signers=1).main()
