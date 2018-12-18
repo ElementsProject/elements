5b6d767b61ee2ac01d1692a2411401d6c76f0bc7
Gregory Sanders <gsanders87@gmail.com>
2018-12-18 13:51:19 -0500

    Load PAK arguments from config and disk on startup

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -40,6 +40,7 @@ void SetupChainParamsBaseOptions()
     gArgs.AddArg("-fedpegscript", "The script for the federated peg.", false, OptionsCategory::CHAINPARAMS);
     gArgs.AddArg("-enforce_pak", "Causes standardness checks to enforce Pegout Authorization Key(PAK) validation, and miner to include PAK commitments when configured. Can not be set when acceptnonstdtx is set to true.", false, OptionsCategory::ELEMENTS);
     gArgs.AddArg("-multi_data_permitted", "Allow relay of multiple OP_RETURN outputs. (default: true)", false, OptionsCategory::ELEMENTS);
+    gArgs.AddArg("-pak", "Entries in the PAK list. Order of entries matter.", false, OptionsCategory::ELEMENTS);
 }
 
 static std::unique_ptr<CBaseChainParams> globalChainBaseParams;
