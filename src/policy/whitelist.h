a2a27c8ad3fa2f44560417d6c7f2dbf79e043e87
Lawrence <lawrence.deacon@gmail.com>
2019-02-04 20:49:15 +0000

    Unwanted changes.

--- src/policy/whitelist.h
+++ src/policy/whitelist.h
@@ -4,19 +4,34 @@
 
 #pragma once
 
-#include "policylist.h"
+#include "policyList.h"
+#include "wallet/wallet.h"
+#include <map>
 
 class CWhiteList : public CPolicyList{
 public:
 	CWhiteList();
 	virtual ~CWhiteList();
 
-	void add_derived(std::string addressIn, std::string key);
+	void add_derived(CBitcoinAddress address, CPubKey pubKey, CBitcoinAddress* kycAddress);
+	void add_derived(CBitcoinAddress address, CPubKey pubKey);
+
+	void add_derived(std::string sAddress, std::string sPubKey, std::string sKYCAddress);
+	void add_derived(std::string sAddress, std::string sKey);
 
 	void synchronise(CWhiteList* wl_new);
 
+  	bool RegisterAddress(const CTransaction& tx, const CCoinsViewCache& mapInputs);
+
+
+  	//Lookup owner (idpubkey) of address
+  	bool LookupKYCKey(const CKeyID& keyId, CKeyID& kycKeyIdFound);
+
 private:
-	//Make add_sorted private because we only want verified derived keys
+	//Make add_sorted private because we only want verified derived keys 
 	//to be added to the CWhiteList.
 	using CPolicyList::add_sorted;
+
+	//A map of address to idPubKey
+	std::map<CKeyID, CKeyID> _kycMap;
 };
