3779ae290d930c33ad81c34ee14e68e9be909b7e
Luke Dashjr <luke-jr+git@utopios.org>
2019-03-04 22:52:20 +0000

    Bugfix: GUI: Re-enable Show/Remove buttons on Receive tab

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -94,10 +94,12 @@ void ReceiveCoinsDialog::setModel(WalletModel *_model)
 #if 0
         tableView->setColumnWidth(RecentRequestsTableModel::Label, LABEL_COLUMN_WIDTH);
         tableView->setColumnWidth(RecentRequestsTableModel::Amount, AMOUNT_MINIMUM_COLUMN_WIDTH);
+#endif
 
         connect(tableView->selectionModel(),
             &QItemSelectionModel::selectionChanged, this,
             &ReceiveCoinsDialog::recentRequestsView_selectionChanged);
+#if 0
         // Last 2 columns are set by the columnResizingFixer, when the table geometry is ready.
         columnResizingFixer = new GUIUtil::TableViewLastColumnResizingFixer(tableView, AMOUNT_MINIMUM_COLUMN_WIDTH, DATE_COLUMN_WIDTH, this);
 #endif
