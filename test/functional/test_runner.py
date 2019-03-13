632b54a7337d3a3c4117c9d97593ee3f743244f2
Gregory Sanders <gsanders87@gmail.com>
2019-03-13 16:10:49 -0400

    re-add reissuance test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -70,6 +70,7 @@ BASE_SCRIPTS = [
     'feature_issuance.py',
     'feature_default_asset_name.py',
     'feature_assetdir.py',
+    'feature_initial_reissuance_token.py',
     # Longest test should go first, to favor running tests in parallel
     'feature_fee_estimation.py',
     'wallet_hd.py',
