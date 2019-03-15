5275d551c075f63acea87600010c7461973fe26b
Gregory Sanders <gsanders87@gmail.com>
2019-03-15 16:07:40 -0400

    Segwit deployed block 0 in liquidv1

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -742,10 +742,15 @@ public:
 
         consensus.subsidy_asset = consensus.pegged_asset;
 
-        // CSV always active by default
+        // CSV always active
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
+        // Segwit, likewise
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
+
 
         // Finally, create genesis block
         genesis = CreateGenesisBlock(consensus, CScript(commit), CScript(OP_RETURN), 1296688602, 2, 0x207fffff, 1, 0);
