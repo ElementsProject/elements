1970fcf4081c66c069e48c31d31aa39d8721877f
Pieter Wuille <pieter.wuille@gmail.com>
2019-04-02 13:12:08 -0700

    Initialize PrecomputedTransactionData in CheckInputs

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -128,6 +128,11 @@ struct PrecomputedTransactionData
     uint256 hashPrevouts, hashSequence, hashOutputs, hashIssuance;
     bool ready = false;
 
+    PrecomputedTransactionData() = default;
+
+    template <class T>
+    void Init(const T& tx);
+
     template <class T>
     explicit PrecomputedTransactionData(const T& tx);
 };
