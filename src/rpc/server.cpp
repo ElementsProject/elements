fa9841b5cf0e398252e09b4b42d3afc15f0cee90
Gregory Sanders <gsanders87@gmail.com>
2019-04-02 08:42:04 -0400

    Fix a few Bitcoin server strings

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -229,11 +229,11 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.fHelp || jsonRequest.params.size() > 1)
         throw std::runtime_error(
             "stop\n"
-            "\nStop Bitcoin server.");
+            "\nStop Elements server.");
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
-    return "Bitcoin server stopping";
+    return "Elements server stopping";
 }
 
 static UniValue uptime(const JSONRPCRequest& jsonRequest)
