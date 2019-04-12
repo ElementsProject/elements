4e149a26b04c0b51a63f1e3cf071d338a4d549c7
Lawrence Deacon <lawrence.deacon@gmail.com>
2019-04-12 10:44:58 +0100

    Debug wallet.cpp - check the unblinded and unsigned tx for policy outputs.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3296,9 +3296,11 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
 
                 // If we made it here and we aren't even able to meet the relay fee on the next pass, give up
                 // because we must be at the maximum allowed fee, if this is not a policy Tx
-                if ((nFeeNeeded < ::minRelayTxFee.GetFee(nBytes)) &! IsAllPolicy(txNew))
+                if ((nFeeNeeded < ::minRelayTxFee.GetFee(nBytes)))
                 {
-                    strFailReason = _("Transaction too large for fee policy");
+                    if(!IsAllPolicy(txUnblindedAndUnsigned)){
+                        strFailReason = _("Transaction too large for fee policy");
+                    }
                     return false;
                 }
 
