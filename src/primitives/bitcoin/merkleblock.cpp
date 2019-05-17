39c2906dcafb19b5b2cf228855e1bc9243217fe5
Steven Roose <steven@stevenroose.org>
2019-05-17 16:54:52 +0100

    MERGE-FIX: Update src/primitives/bitcoin/

--- src/primitives/bitcoin/merkleblock.cpp
+++ src/primitives/bitcoin/merkleblock.cpp
@@ -55,7 +55,7 @@ uint256 CPartialMerkleTree::CalcHash(int height, unsigned int pos, const std::ve
         else
             right = left;
         // combine subhashes
-        return Hash(BEGIN(left), END(left), BEGIN(right), END(right));
+        return Hash(left.begin(), left.end(), right.begin(), right.end());
     }
 }
 
@@ -111,7 +111,7 @@ uint256 CPartialMerkleTree::TraverseAndExtract(int height, unsigned int pos, uns
             right = left;
         }
         // and combine them before returning
-        return Hash(BEGIN(left), END(left), BEGIN(right), END(right));
+        return Hash(left.begin(), left.end(), right.begin(), right.end());
     }
 }
 
