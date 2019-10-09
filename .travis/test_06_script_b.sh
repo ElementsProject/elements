d71ddbee40ecb3bae52b449c3f6a6f35c645e24e
Gregory Sanders <gsanders87@gmail.com>
2019-10-09 17:16:04 -0400

    Don't run normal functional tests twice.

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -25,3 +25,29 @@ if [ "$RUN_FUZZ_TESTS" = "true" ]; then
   DOCKER_EXEC test/fuzz/test_runner.py -l DEBUG ${DIR_FUZZ_IN}
   END_FOLD
 fi
+
+if [ "$RUN_UNIT_TESTS" = "true" ]; then
+  BEGIN_FOLD unit-tests
+  DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1
+  END_FOLD
+fi
+
+if [ "$RUN_BITCOIN_TESTS" = "true" ]; then
+    BEGIN_FOLD bitcoin-functional-tests
+    DOCKER_EXEC test/bitcoin_functional/functional/test_runner.py --combinedlogslen=4000 --coverage --quiet --failfast ${extended}
+    END_FOLD
+fi
+
+if [ "$RUN_FEDPEG_BITCOIND_TEST" = "true" ]; then
+    BEGIN_FOLD fedpeg-bitcoind-test
+    BITCOIND_VERSION=0.18.0
+    BITCOIND_ARCH=x86_64-linux-gnu
+    DOCKER_EXEC curl -O https://bitcoincore.org/bin/bitcoin-core-$BITCOIND_VERSION/bitcoin-$BITCOIND_VERSION-$BITCOIND_ARCH.tar.gz
+    DOCKER_EXEC tar -zxf bitcoin-$BITCOIND_VERSION-$BITCOIND_ARCH.tar.gz
+    DOCKER_EXEC test/functional/feature_fedpeg.py --parent_bitcoin --parent_binpath $(pwd)/bitcoin-$BITCOIND_VERSION/bin/bitcoind
+    END_FOLD
+fi
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+  extended="--extended --exclude feature_pruning"
+fi
