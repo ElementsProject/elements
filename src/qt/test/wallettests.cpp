ad8df8d5b23e1424a53638af64477030da906873
Andrew Poelstra <apoelstra@wpsoftware.net>
2020-12-09 21:08:35 +0000

    ci: eliminate a bunch of build warnings
    Includes removing some variables from qt/intro.cpp that've been
    unused since #13216 and ought to have been removed in the 0.17
    rebase, but our linters were not so agressive then. Similarly
    fix the BITCOIN_PID_FILE, whose value was incorrectly changed
    in the 0.18 rebase.

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -226,11 +226,11 @@ void TestGUI(interfaces::Node& node)
     int initialRowCount = requestTableModel->rowCount({});
     QPushButton* requestPaymentButton = receiveCoinsDialog.findChild<QPushButton*>("receiveButton");
     requestPaymentButton->click();
+    /* URI are disabled for Elements-QT
     for (QWidget* widget : QApplication::topLevelWidgets()) {
         if (widget->inherits("ReceiveRequestDialog")) {
 
             ReceiveRequestDialog* receiveRequestDialog = qobject_cast<ReceiveRequestDialog*>(widget);
-            /* URI are disabled for Elements-QT
             QCOMPARE(receiveRequestDialog->QObject::findChild<QLabel*>("payment_header")->text(), QString("Payment information"));
             QCOMPARE(receiveRequestDialog->QObject::findChild<QLabel*>("uri_tag")->text(), QString("URI:"));
             QString uri = receiveRequestDialog->QObject::findChild<QLabel*>("uri_content")->text();
@@ -248,9 +248,9 @@ void TestGUI(interfaces::Node& node)
             QCOMPARE(uri.count("message=TEST_MESSAGE_1"), 2);
             QCOMPARE(receiveRequestDialog->QObject::findChild<QLabel*>("message_tag")->text(), QString("Message:"));
             QCOMPARE(receiveRequestDialog->QObject::findChild<QLabel*>("message_content")->text(), QString("TEST_MESSAGE_1"));
-            */
         }
     }
+    */
 
     // Clear button
     QPushButton* clearButton = receiveCoinsDialog.findChild<QPushButton*>("clearButton");
