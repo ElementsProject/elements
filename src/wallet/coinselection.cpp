efac8a33fabc4aa28b53c8f53bc586f86e11745f
Andrew Chow <achow101-github@achow101.com>
2021-08-11 22:03:56 -0400

    wallet: Use GetSelectionAmount for target value calculations
    For target value calculations, GetSelectionAmount should be used, not
    m_effective_value or m_value.
    
    Specifically, ApproximateBestSubset mistakenly uses m_value when
    calculating whether the target value has been met. This has been changed
    to use GetSelectionAmount.
    
    Github-Pull: bitcoin/bitcoin#22686
    Rebased-From: 2de222c40198d3d528668d78cc52e2ce3fa96765

--- src/wallet/coinselection.cpp
+++ src/wallet/coinselection.cpp
@@ -212,7 +212,7 @@ static void ApproximateBestSubset(const std::vector<OutputGroup>& groups, const 
                 //the selection random.
                 if (nPass == 0 ? insecure_rand.randbool() : !vfIncluded[i])
                 {
-                    nTotal += groups[i].m_value;
+                    nTotal += groups[i].GetSelectionAmount();
                     vfIncluded[i] = true;
                     if (nTotal >= nTargetValue)
                     {
@@ -222,7 +222,7 @@ static void ApproximateBestSubset(const std::vector<OutputGroup>& groups, const 
                             nBest = nTotal;
                             vfBest = vfIncluded;
                         }
-                        nTotal -= groups[i].m_value;
+                        nTotal -= groups[i].GetSelectionAmount();
                         vfIncluded[i] = false;
                     }
                 }
