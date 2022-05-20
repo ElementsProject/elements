9554211b8cd42ed465f21028d1ec1654cf44ad16
Andrea Bonel <andrea@bonel.com.ar>
2022-05-20 11:18:23 -0300

    Source code URL correction in Help-AboutElementsCore elements-qt

--- src/init.cpp
+++ src/init.cpp
@@ -645,7 +645,7 @@ void SetupServerArgs(NodeContext& node)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/ElementsProject/elements>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
