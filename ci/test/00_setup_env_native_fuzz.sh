6be157e26b4a4c287eff1f0bafae7c5993816dbe
Glenn Willen <gwillen@nerdnet.org>
2022-05-25 19:47:48 -0700

    ci: Disable one of the fuzz tests to work around bitcoin#22233

--- ci/test/00_setup_env_native_fuzz.sh
+++ ci/test/00_setup_env_native_fuzz.sh
@@ -13,6 +13,7 @@ export NO_DEPENDS=1
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export RUN_FUZZ_TESTS=true
+export FUZZ_TESTS_CONFIG="--exclude=coins_view"  # work around https://github.com/bitcoin/bitcoin/issues/22233
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer,address,undefined,integer CC=clang CXX=clang++"
 export CCACHE_SIZE=200M
