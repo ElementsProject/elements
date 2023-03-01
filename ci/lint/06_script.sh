65bdef4b5fee3dddae45d72601687ec03cfcb28a
Glenn Willen <gwillen@nerdnet.org>
2023-03-01 12:08:03 -0800

    Use git --no-pager in ci script.
    When running 'git log' in a ci script, pass --no-pager to ensure
    that a pager (i.e. 'less') is never used. When running in CI,
    there is no controlling terminal, so a pager will never be used;
    but when running the CI scripts by hand to reproduce CI issues,
    the automatic invocation of a pager by 'git log' breaks the script.

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -24,10 +24,10 @@ test/lint/check-doc.py
 test/lint/lint-all.sh
 
 if [ "$CIRRUS_REPO_FULL_NAME" = "bitcoin/bitcoin" ] && [ -n "$CIRRUS_CRON" ]; then
-    git log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
+    git --no-pager log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
     ${CI_RETRY_EXE}  gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $(<contrib/verify-commits/trusted-keys) &&
     ./contrib/verify-commits/verify-commits.py --clean-merge=2;
 fi
 
 echo
-git log --no-merges --oneline $COMMIT_RANGE
+git --no-pager log --no-merges --oneline $COMMIT_RANGE
