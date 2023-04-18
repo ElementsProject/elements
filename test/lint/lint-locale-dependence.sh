5cc9caaa03a469d9e43b3956344c41c1dfd467af
Russell O'Connor <roconnor@blockstream.io>
2023-04-18 08:57:46 -0400

    Exclude simplicity directory

--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -48,7 +48,7 @@ KNOWN_VIOLATIONS=(
     "src/test/util_tests.cpp"
 )
 
-REGEXP_IGNORE_EXTERNAL_DEPENDENCIES="^src/(crypto/ctaes/|leveldb/|secp256k1/|minisketch/|tinyformat.h|univalue/)"
+REGEXP_IGNORE_EXTERNAL_DEPENDENCIES="^src/(crypto/ctaes/|leveldb/|secp256k1/|simplicity/|minisketch/|tinyformat.h|univalue/)"
 
 LOCALE_DEPENDENT_FUNCTIONS=(
     alphasort    # LC_COLLATE (via strcoll)
