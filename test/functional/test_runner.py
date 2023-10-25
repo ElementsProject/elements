e02024e53fa1a7501222f868a7524a1259186071
James Dorfman <dorfmanjames@gmail.com>
2023-10-25 17:02:35 +0000

    test: add new elements_code_tutorial functional test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -88,6 +88,7 @@ EXTENDED_SCRIPTS = [
 BASE_SCRIPTS = [
     # Scripts that are run by default.
     # vv First elements tests vv
+    'example_elements_code_tutorial.py',
     'feature_fedpeg.py --legacy-wallet',
     'feature_fedpeg.py --pre_transition --legacy-wallet',
     'feature_fedpeg.py --post_transition --legacy-wallet',
