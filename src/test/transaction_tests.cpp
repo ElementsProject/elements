d070b5d5dcd8987340c02285107187d765b76317
Byron Hambly <bhambly@blockstream.com>
2026-03-25 15:09:35 +0200

    test: added exact annex padding test for simplicity spends

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -65,6 +65,7 @@ static std::map<std::string, unsigned int> mapFlagNames = {
     {std::string("DISCOURAGE_OP_SUCCESS"), (unsigned int)SCRIPT_VERIFY_DISCOURAGE_OP_SUCCESS},
     {std::string("DISCOURAGE_UPGRADABLE_TAPROOT_VERSION"), (unsigned int)SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_TAPROOT_VERSION},
     {std::string("SIMPLICITY"), (unsigned int)SCRIPT_VERIFY_SIMPLICITY},
+    {std::string("ANNEX_PADDING"), (unsigned int)SCRIPT_VERIFY_ANNEX_PADDING},
 };
 
 unsigned int ParseScriptFlags(std::string strFlags)
