7577eb93a6ab3ac3f2540376b1a8c3f60ca93dfe
Pablo Greco <psgreco@gmail.com>
2026-05-27 05:27:42 -0700

    Enable ELIP203 by default in liquidtestnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1108,6 +1108,7 @@ public:
         default_signblockscript = "51210217e403ddb181872c32a0cd468c710040b2f53d8cac69f18dad07985ee37e9a7151ae";
         create_discount_ct = false;
         accept_discount_ct = true;
+        accept_unlimited_issuances = true;
         UpdateFromArgs(args);
         multi_data_permitted = true;
         SetGenesisBlock();
